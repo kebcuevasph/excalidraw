ebaa73503768de9586a476291b9606a2d82b91d6
Keb Cuevas <51538653+kebcuevasph@users.noreply.github.com>
2025-08-04 20:54:33 +0800

    language remove

--- excalidraw-app/components/LanguageList.tsx
+++ excalidraw-app/components/LanguageList.tsx
@@ -1,26 +1,10 @@
-import { useSetAtom } from "jotai";
 import React from "react";
+import { useSetAtom } from "jotai";
 import { appLangCodeAtom } from "..";
 import { useI18n } from "../../src/i18n";
 import { languages } from "../../src/i18n";
 
 export const LanguageList = ({ style }: { style?: React.CSSProperties }) => {
-  const { t, langCode } = useI18n();
-  const setLangCode = useSetAtom(appLangCodeAtom);
-
-  return (
-    <select
-      className="dropdown-select dropdown-select__language"
-      onChange={({ target }) => setLangCode(target.value)}
-      value={langCode}
-      aria-label={t("buttons.selectLanguage")}
-      style={style}
-    >
-      {languages.map((lang) => (
-        <option key={lang.code} value={lang.code}>
-          {lang.label}
-        </option>
-      ))}
-    </select>
-  );
+  // Returning null effectively removes this component from the UI.
+  return null;
 };

22738e59c6fc547a04c29b56e3832717baa5edfa
Keb Cuevas <51538653+kebcuevasph@users.noreply.github.com>
2025-08-04 16:35:33 +0800

    fixes

--- excalidraw-app/collab/RoomDialog.tsx
+++ excalidraw-app/collab/RoomDialog.tsx
@@ -20,7 +20,7 @@ import {
 import { TextField } from "../../src/components/TextField";
 import { FilledButton } from "../../src/components/FilledButton";
 
-import { ReactComponent as CollabImage } from "../../src/assets/lock.svg";
+// import { ReactComponent as CollabImage } from "../../src/assets/lock.svg";
 import "./RoomDialog.scss";
 
 const getShareIcon = () => {
@@ -180,7 +180,6 @@ export const RoomModal = ({
   return (
     <>
       <div className="RoomDialog__inactive__illustration">
-        <CollabImage />
       </div>
       <div className="RoomDialog__inactive__header">
         {t("labels.liveCollaboration")}
